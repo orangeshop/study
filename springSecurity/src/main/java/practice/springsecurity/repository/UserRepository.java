62de4b1f010f7b9b852d6fec87c2133658214c64
orangeshop <rhkeh123@naver.com>
2025-07-07 16:52:40 +0900

    이전 학습 파일 삭제

--- springSecurity/src/main/java/practice/springsecurity/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package practice.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import practice.springsecurity.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- springSecurity/src/main/java/practice/springsecurity/service/UserService.java
+++
@@ -1,5 +0,0 @@
-package practice.springsecurity.service;
-
-public interface UserService {
-
-}
